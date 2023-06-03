614147450a6ebc1ccc2951e4e047a812f8dfb375
Csongor Dobesch <csongordobesch@gmail.com>
2023-06-03 13:27:01 +0200

    C1

--- Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
+++ Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hubsson.Hackathon.Arcade.Client.Dotnet.Contracts;
+using System;
 using ClientGameState = Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.ClientGameState;
 
 namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
@@ -24,12 +25,17 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         {
             // On Each Frame Update return an Action for your player
 
-            throw new NotImplementedException();
+            MatchRepository matchRepository = _matchRepository;
+            matchRepository.PlayerCords = gameState.players;
+            
+            return new Domain.Action() { direction= Domain.Direction.Right, iteration = gameState.iteration };
         }
 
         private class MatchRepository
         {
             // Write your data fields here what you would like to store between the match rounds
+            public PlayerCoordinates[] PlayerCords { get; set; }
+
         }
     }
 }
